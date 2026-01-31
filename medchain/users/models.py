2a558c1aebc8e4e71f2b9a6b9d7ba55bc70f8e7b
Naseer-010 <naseer20704446@gmail.com>
2026-01-31 19:29:48 +0530

    Initial setup and user registeration

---
+++ medchain/users/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('PATIENT', 'Patient'),
+        ('DOCTOR', 'Doctor'),
+    )
+
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    wallet_address = models.CharField(max_length=255, blank=True, null=True)
+    is_verified_doctor = models.BooleanField(default=False)
